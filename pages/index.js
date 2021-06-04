8e60fb62ce5b6b19afa745ffabdc8bce41f44f00
zlajo-outfit <58464865+zlajo-outfit@users.noreply.github.com>
2021-06-04 11:45:15 +0200

    Initial Commit

---
+++ pages/index.js
@@ -0,0 +1,24 @@
+import fs from 'fs/promises';
+import path from 'path';
+
+function HomePage(props) {
+  const {products} = props;
+  return (
+    <ul>
+      {products.map((product) => 
+      <li key={product.id}>{product.title}</li>
+      )}
+    </ul>
+  );
+}
+
+export async function getStaticProps() {
+  const filePath = path.join(process.cwd(), 'data', 'dummy-backend.json');
+  const jsonData = await fs.readFile(filePath);
+  const data = JSON.parse(jsonData);
+  return { props: {
+    products: data.products
+  }};
+}
+
+export default HomePage;
